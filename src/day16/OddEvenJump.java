988691a5fa67da862ec528019f1d7fe5d1e96cd3
amitsupercomp <amitsupercomp@gmail.com>
2024-02-29 23:41:17 +0530

    day16

---
+++ src/day16/OddEvenJump.java
@@ -0,0 +1,30 @@
+package day16;
+
+import java.util.*;
+
+public class OddEvenJump {
+
+	private static int oddEvenJumps(int[] arr) {
+		int n  = arr.length, res = 1;
+		boolean[] higher = new boolean[n], lower = new boolean[n];
+		higher[n - 1] = lower[n - 1] = true;
+		TreeMap<Integer, Integer> map = new TreeMap<>();
+		map.put(arr[n - 1], n - 1);
+		for (int i = n - 2; i >= 0; --i) {
+			Map.Entry<Integer, Integer> hi = map.ceilingEntry(arr[i]), lo = map.floorEntry(arr[i]);
+			if (hi != null) higher[i] = lower[(int)hi.getValue()];
+			if (lo != null) lower[i] = higher[(int)lo.getValue()];
+			if (higher[i]) res++;
+			map.put(arr[i], i);
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {10,13,12,14,15};
+		
+		System.out.println("Good Starting index :: " + oddEvenJumps(arr));
+		
+	}
+
+}
