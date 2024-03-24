12c3c90c328254437636ca33f5a052e47e152ed7
amitsupercomp <amitsupercomp@gmail.com>
2024-03-24 22:54:36 +0530

    day40

--- README.md
+++ README.md
@@ -1236,3 +1236,28 @@ Output: [1,4,2,3]
 Example 2:
 Input: head = [1,2,3,4,5]
 Output: [1,5,2,4,3]
+
+
+
+
+Day-40
+-----------
+Find the Duplicate Number
+------------------------------
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+Example 1:
+Input: nums = [1,3,4,2,2]
+Output: 2
+
+Example 2:
+Input: nums = [3,1,3,4,2]
+Output: 3
+
+Example 3:
+Input: nums = [3,3,3,3,3]
+Output: 3
---
+++ src/day40/DuplicateNumber.java
@@ -0,0 +1,54 @@
+package day40;
+import java.util.*;
+
+public class DuplicateNumber {
+	
+	public static void main(String[] args) {
+		int[] nums = {1,3,4,2,2};
+		System.out.println("Duplicate Number ::  " + findDuplicate(nums));
+		System.out.println("Duplicate Number ::  " + findDuplicate1(nums));
+		System.out.println("Duplicate Number ::  " + findDuplicate2(nums));
+		System.out.println("Duplicate Number ::  " + findDuplicate3(nums));
+	}
+
+	private static int findDuplicate(int[] nums) {
+		boolean[] present = new boolean[nums.length];
+		for (int num : nums) {
+			if (present[num])
+				return num;
+			present[num] = true;
+		}
+		return -1;
+	}
+
+	private static int findDuplicate1(int[] nums) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			if (map.containsKey(nums[i]))
+				return nums[i];
+			else
+				map.put(nums[i], 1);
+		}
+		return -1;
+	}
+
+	private static int findDuplicate2(int[] nums) {
+		Arrays.sort(nums);
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] == nums[i + 1])
+				return nums[i];
+		}
+		return -1;
+	}
+
+	private static int findDuplicate3(int[] nums) {
+		int dupl = nums[0], temp = 0;
+		while (dupl != nums[dupl]) {
+			temp = nums[dupl];
+			nums[dupl] = dupl;
+			dupl = temp;
+		}
+		return dupl;
+	}
+
+}
