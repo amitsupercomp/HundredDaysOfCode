cf831dd539e69ae7f56e70eb3bb26b8653263897
amitsupercomp <amitsupercomp@gmail.com>
2024-04-27 22:41:33 +0530

    day74

---
+++ src/day74/FreedomTrail.java
@@ -0,0 +1,35 @@
+package day74;
+import java.util.*;
+
+public class FreedomTrail {
+	
+	public static void main(String[] args) {
+		String ring = "godding", key = "gd";
+		System.out.println("OutPut :: " + findRotateSteps(ring, key));
+	}
+	
+	private static int findRotateSteps(String ring, String key) {
+        char[] r=ring.toCharArray();
+        List<Integer>[] p=new List[26];
+        for(int i=0;i<r.length;i++) {
+            int c=r[i]-'a';
+            List<Integer> l=p[c];
+            if(l==null) p[c]=l=new ArrayList<>();
+            l.add(i);
+        }
+        return helper(0,0,p,key.toCharArray(),ring,new int[key.length()][r.length]);
+    }
+    private static int helper(int in, int pos, List<Integer>[] p, char[] k, String r, int[][] memo) {
+        if(in==k.length) return 0;
+        if(memo[in][pos]>0) return memo[in][pos]-1;
+        int min=Integer.MAX_VALUE;
+        for(int i: p[k[in]-'a']) {
+            int m;
+            if(i>=pos) m=Math.min(i-pos,pos+r.length()-i);
+            else m=Math.min(pos-i,i+r.length()-pos);
+            min=Math.min(min,m+helper(in+1,i,p,k,r,memo));
+        }
+        return (memo[in][pos]=min+2)-1;
+    }
+
+}
