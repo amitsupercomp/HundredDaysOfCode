01c032b4b0809d0ada33c34f4c921e7754b70719
amitsupercomp <amitsupercomp@gmail.com>
2024-05-08 00:17:21 +0530

    day84

--- README.md
+++ README.md
@@ -2563,3 +2563,23 @@ Example 2:
 Input: head = [1,1,1,1]
 Output: [1,1,1,1]
 Explanation: Every node has value 1, so no nodes are removed.
+
+
+
+Day-84
+-------
+Double a Number Represented as a Linked List
+-----------------------------------------------
+You are given the head of a non-empty linked list representing a non-negative integer without leading zeroes.
+
+Return the head of the linked list after doubling it.
+
+Example 1:
+Input: head = [1,8,9]
+Output: [3,7,8]
+Explanation: The figure above corresponds to the given linked list which represents the number 189. Hence, the returned linked list represents the number 189 * 2 = 378.
+
+Example 2:
+Input: head = [9,9,9]
+Output: [1,9,9,8]
+Explanation: The figure above corresponds to the given linked list which represents the number 999. Hence, the returned linked list reprersents the number 999 * 2 = 1998. 
---
+++ src/day84/DoubleNuberLinkedList.java
@@ -0,0 +1,54 @@
+package day84;
+
+import day37.ReverseLinkedList.ListNode;
+
+public class DoubleNuberLinkedList {
+
+	public static class ListNode {
+		int val;
+		ListNode next;
+		ListNode() {}
+		ListNode(int val) { this.val = val; }
+		ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	}
+
+	public static void main(String[] args) {
+		ListNode head = new ListNode(9); 
+		head.next = new ListNode(9); 
+		head.next.next = new ListNode(9); 
+		head.next.next.next = new ListNode(2); 
+		head.next.next.next.next = new ListNode(5); 
+		head.next.next.next.next.next = new ListNode(6); 
+		head.next.next.next.next.next.next = new ListNode(7);
+		System.out.println("Original list is :: ");
+		ListNode ret = head;
+		while (ret != null) {
+			System.out.print(ret.val + ", ");
+			ret = ret.next;
+		}
+		System.out.println("\n Result List :: ");
+		ListNode result = doubleIt(head);
+		while (result != null) {
+			System.out.print(result.val + ", ");
+			result = result.next;
+		}
+	}
+
+	private static ListNode doubleIt(ListNode head) {
+		ListNode head1 = new ListNode();
+		head1.next = head;
+
+		ListNode left = head1;
+		ListNode right = head;
+		while (right != null) {
+			if (right.val * 2 >= 10) {
+				left.val += 1;
+			}
+			right.val = (right.val * 2) % 10;
+			left = left.next;
+			right = right.next;
+		}
+		return head1.val > 0 ? head1 : head;
+	}
+
+}
